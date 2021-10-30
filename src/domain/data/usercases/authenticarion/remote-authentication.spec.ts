f23c60b767705fa41bb1b7db73a6f18d4cb02368
Joao-Correia <joao.correia.marcos@gmail.com>
2021-10-30 16:44:28 -0300

    refactor: move SUT for makeSut method

--- src/domain/data/usercases/authenticarion/remote-authentication.spec.ts
+++ src/domain/data/usercases/authenticarion/remote-authentication.spec.ts
@@ -1,10 +1,24 @@
 import { RemoteAuthentication } from './remote-authentication'
 import { HttpPostClientSpy } from '../../test/moke-http-client'
+
+type SubTypes = {
+  sut: RemoteAuthentication
+  httpPostClientSpy: HttpPostClientSpy
+}
+
+const makeSut = (url: string = ''): SubTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy()
+  const sut = new RemoteAuthentication(url, httpPostClientSpy)
+  return {
+    sut,
+    httpPostClientSpy
+  }
+}
+
 describe('RemoteAuthentication', () => {
   test('Should call HttpClient with Correct URL', async () => {
-    const url = 'any_url'
-    const httpPostClientSpy = new HttpPostClientSpy()
-    const sut = new RemoteAuthentication(url, httpPostClientSpy)
+    const url = 'other_url'
+    const { sut, httpPostClientSpy } = makeSut(url)
     await sut.auth()
     expect(httpPostClientSpy.url).toBe(url)
   })
