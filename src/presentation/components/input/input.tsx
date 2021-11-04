736fddc1cec75d71a5b2e4bea47a4c87af07f963
Joao-Correia <joao.correia.marcos@gmail.com>
2021-11-03 21:07:44 -0300

    feat: add remove autofill input on chorme.

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -5,10 +5,15 @@ import { GrStatusGoodSmall } from 'react-icons/gr'
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
+    const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
+        event.target.readOnly = false
+    }
     return (
         <div className={Styles.inputWrap}>
-            <input {...props} />
-            <span className={Styles.status}><GrStatusGoodSmall color="#791500" /></span>
+            <input {...props} readOnly onFocus={enableInput} />
+            <span className={Styles.status}>
+                <GrStatusGoodSmall color="#791500" />
+            </span>
         </div>
     )
 }
