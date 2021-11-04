8ed7d88d5aa2a7cab06a7310bf50af1f4e01f463
Joao-Correia <joao.correia.marcos@gmail.com>
2021-11-03 21:01:12 -0300

    feat: add app router and global styles.

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Styles from './input-styles.scss'
+import { GrStatusGoodSmall } from 'react-icons/gr'
 
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
@@ -7,7 +8,7 @@ const Input: React.FC<Props> = (props: Props) => {
     return (
         <div className={Styles.inputWrap}>
             <input {...props} />
-            <span className={Styles.status}>*</span>
+            <span className={Styles.status}><GrStatusGoodSmall color="#791500" /></span>
         </div>
     )
 }
---
+++ src/presentation/components/router/router.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import '@/presentation/styles/global.scss'
+
+import { Login } from '../../pages'
+
+const Router: React.FC = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Router
