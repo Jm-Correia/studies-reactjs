2daf02e0509023b8658985f315ca76711f64fc14
Joao-Correia <joao.correia.marcos@gmail.com>
2021-11-09 20:23:42 -0400

    test: ensure validation build return list of validations

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -6,18 +6,28 @@ import RequiredFieldValidation from '../required-field/required-field-validation
 describe('ValidationBuilder', () => {
     it('Should return RequiredFieldValidation', () => {
         const validations = ValidationBuilder.field('any_field').required().build()
-        expect(validations.length).toBe(1)
+        expect(validations).toHaveLength(1)
         expect(validations).toEqual([new RequiredFieldValidation('any_field')])
     })
 
     it('Should return EmailValidation', () => {
         const validations = ValidationBuilder.field('email').email().build()
-        expect(validations.length).toBe(1)
+        expect(validations).toHaveLength(1)
         expect(validations).toEqual([new EmailValidation('email')])
     })
     it('Should return MinLenghValidation', () => {
         const validations = ValidationBuilder.field('email').min(3).build()
-        expect(validations.length).toBe(1)
+        expect(validations).toHaveLength(1)
         expect(validations).toEqual([new MinLenghValidation('email', 3)])
+    })
+
+    it('Should return list of validations', () => {
+        const validations = ValidationBuilder.field('email').required().min(3).email().build()
+        expect(validations).toHaveLength(3)
+        expect(validations).toEqual([
+            new RequiredFieldValidation('email'),
+            new MinLenghValidation('email', 3),
+            new EmailValidation('email')
+        ])
     })
 })
